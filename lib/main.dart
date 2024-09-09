8dce9e5abaa8a4c226e561105f43349eab638729
mohandElkosh <mokelkosh@gmail.com>
2024-09-09 12:06:41 -0700

    add firebase and it's packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:chat_pro/main_screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final savedThemeMode = await AdaptiveTheme.getThemeMode();
   runApp(MyApp(savedThemeMode: savedThemeMode));
 }
